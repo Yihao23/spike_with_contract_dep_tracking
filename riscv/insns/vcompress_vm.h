riscv-software-src/riscv-isa-sim

rvv: add reg checking for specifial instructions
include:
    1. move
    2.

Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

riscv/insns/vcompress_vm.h
@@ -1,14 +1,13 @@
 // vcompress vd, vs2, vs1
-require(P.VU.vsew >= e8 && P.VU.vsew <= e64);
-require_vector;
 require(P.VU.vstart == 0);
-reg_t sew = P.VU.vsew;
-reg_t vl = P.VU.vl;
-reg_t rd_num = insn.rd();
-reg_t rs1_num = insn.rs1();
-reg_t rs2_num = insn.rs2();
+require((insn.rd() & (P.VU.vlmul - 1)) == 0);
+require((insn.rs2() & (P.VU.vlmul - 1)) == 0);
+require(insn.rd() != insn.rs2());
+require(!is_overlaped(insn.rd(), P.VU.vlmul, insn.rs1(), 1));
+
 reg_t pos = 0;
-for (reg_t i = P.VU.vstart ; i < vl; ++i) {
+
+VI_GENERAL_LOOP_BASE
   const int mlen = P.VU.vmlen;
   const int midx = (mlen * i) / 64;
   const int mpos = (mlen * i) % 64;
@@ -32,10 +31,4 @@ for (reg_t i = P.VU.vstart ; i < vl; ++i) {
 
     ++pos;
   }
-}
-
-if (vl > 0 && P.VU.TZ) {
-  uint8_t *tail = &P.VU.elt<uint8_t>(rd_num, pos * ((sew >> 3) * 1));
-  memset(tail, 0, (P.VU.vlmax - pos) * ((sew >> 3) * 1));
-}
-
+VI_LOOP_END;

riscv/insns/viota_m.h
@@ -7,6 +7,10 @@ reg_t rd_num = insn.rd();
 reg_t rs1_num = insn.rs1();
 reg_t rs2_num = insn.rs2();
 require(P.VU.vstart == 0);
+require(!is_overlaped(rd_num, P.VU.vlmul, rs2_num, 1));
+if (insn.v_vm() == 0)
+  require(!is_overlaped(rd_num, P.VU.vlmul, 0, 1));
+require((rd_num & (P.VU.vlmul - 1)) == 0);
 
 int cnt = 0;
 for (reg_t i = 0; i < vl; ++i) {

Yihao23/spike_with_contract_dep_tracking

riscv/insns/vcompress_vm.h
@@ -1,14 +1,13 @@
 // vcompress vd, vs2, vs1
-require(P.VU.vsew >= e8 && P.VU.vsew <= e64);
-require_vector;
 require(P.VU.vstart == 0);
-reg_t sew = P.VU.vsew;
-reg_t vl = P.VU.vl;
-reg_t rd_num = insn.rd();
-reg_t rs1_num = insn.rs1();
-reg_t rs2_num = insn.rs2();
+require((insn.rd() & (P.VU.vlmul - 1)) == 0);
+require((insn.rs2() & (P.VU.vlmul - 1)) == 0);
+require(insn.rd() != insn.rs2());
+require(!is_overlapped(insn.rd(), P.VU.vlmul, insn.rs1(), 1));
+
 reg_t pos = 0;
-for (reg_t i = P.VU.vstart ; i < vl; ++i) {
+
+VI_GENERAL_LOOP_BASE
   const int mlen = P.VU.vmlen;
   const int midx = (mlen * i) / 64;
   const int mpos = (mlen * i) % 64;
@@ -32,10 +31,4 @@ for (reg_t i = P.VU.vstart ; i < vl; ++i) {
 
     ++pos;
   }
-}
-
-if (vl > 0 && P.VU.TZ) {
-  uint8_t *tail = &P.VU.elt<uint8_t>(rd_num, pos * ((sew >> 3) * 1));
-  memset(tail, 0, (P.VU.vlmax - pos) * ((sew >> 3) * 1));
-}
-
+VI_LOOP_END;

riscv/insns/viota_m.h
@@ -7,6 +7,10 @@ reg_t rd_num = insn.rd();
 reg_t rs1_num = insn.rs1();
 reg_t rs2_num = insn.rs2();
 require(P.VU.vstart == 0);
+require(!is_overlapped(rd_num, P.VU.vlmul, rs2_num, 1));
+if (insn.v_vm() == 0)
+  require(!is_overlapped(rd_num, P.VU.vlmul, 0, 1));
+require((rd_num & (P.VU.vlmul - 1)) == 0);
 
 int cnt = 0;
 for (reg_t i = 0; i < vl; ++i) {