riscv-software-src/riscv-isa-sim

commit cb254bfab5fbb6d69e5dd336798861e7c1183e52

Initialize PMPs with set_csr to fix WARLness of initial value

riscv/processor.cc
@@ -254,10 +254,7 @@ void state_t::reset(reg_t max_isa)
   debug_mode = false;
 
   memset(this->pmpcfg, 0, sizeof(this->pmpcfg));
-  pmpcfg[0] = PMP_R | PMP_W | PMP_X | PMP_NAPOT;
-
   memset(this->pmpaddr, 0, sizeof(this->pmpaddr));
-  pmpaddr[0] = ~reg_t(0);
 
   fflags = 0;
   frm = 0;
@@ -351,6 +348,12 @@ void processor_t::set_log_commits(bool value)
 void processor_t::reset()
 {
   state.reset(max_isa);
+
+  // For backwards compatibility with software that is unaware of PMP,
+  // initialize PMP to permit unprivileged access to all of memory.
+  set_csr(CSR_PMPADDR0, ~reg_t(0));
+  set_csr(CSR_PMPCFG0, PMP_R | PMP_W | PMP_X | PMP_NAPOT);
+
   state.dcsr.halt = halt_on_reset;
   halt_on_reset = false;
   set_csr(CSR_MSTATUS, state.mstatus);

Yihao23/spike_with_contract_dep_tracking

commit 5d93ba189013e4791caee04902fe5637d7527691

Fix pmpcfg0 initial value regression from cb254bfab5fbb6d69e5dd336798861e7c1183e52

riscv/processor.cc
@@ -349,15 +349,15 @@ void processor_t::reset()
 {
   state.reset(max_isa);
 
-  // For backwards compatibility with software that is unaware of PMP,
-  // initialize PMP to permit unprivileged access to all of memory.
-  set_csr(CSR_PMPADDR0, ~reg_t(0));
-  set_csr(CSR_PMPCFG0, PMP_R | PMP_W | PMP_X | PMP_NAPOT);
-
   state.dcsr.halt = halt_on_reset;
   halt_on_reset = false;
   set_csr(CSR_MSTATUS, state.mstatus);
   VU.reset();
+
+  // For backwards compatibility with software that is unaware of PMP,
+  // initialize PMP to permit unprivileged access to all of memory.
+  set_csr(CSR_PMPADDR0, ~reg_t(0));
+  set_csr(CSR_PMPCFG0, PMP_R | PMP_W | PMP_X | PMP_NAPOT);
 
   if (ext)
     ext->reset(); // reset the extension