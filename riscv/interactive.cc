riscv/riscv-isa-sim

functions needed for socket input/output

riscv/interactive.cc
@@ -8,6 +8,7 @@
 #include <termios.h>
 #include <map>
 #include <iostream>
+#include <iomanip>
 #include <climits>
 #include <cinttypes>
 #include <assert.h>
@@ -18,6 +19,19 @@
 #include <vector>
 #include <algorithm>
 #include <math.h>
+
+using std::ostream;
+using std::cerr;
+using std::hex;
+using std::dec;
+using std::setfill;
+using std::left;
+using std::right;
+using std::setw;
+using std::endl;
+
+#define STR_(X) #X      // these definitions allow to use a macro as a string
+#define STR(X) STR_(X)
 
 DECLARE_TRAP(-1, interactive)
 
@@ -58,10 +72,67 @@ static std::string readline(int fd)
   return s;
 }
 
+#ifdef HAVE_BOOST
+// read input command string
+std::string sim_t::rin(streambuf *bout_ptr) {
+   std::string s;
+   if (acceptor_ptr) { // if we are listening, get commands from socket
+      try
+      {
+         socket_ptr = new tcp::socket(*io_service_ptr);
+         acceptor_ptr->accept(*socket_ptr); // wait for someone to open connection
+         boost::asio::streambuf buf;
+         boost::asio::read_until(*socket_ptr, buf, "\n"); // wait for command
+         s = buffer_cast<const char*>(buf.data());
+         erase_all(s, "\r");  // get rid off any cr and lf
+         erase_all(s, "\n");
+         // The socket client is a web server and it appends the IP of the computer
+         // that sent the command from its web browser.
+
+         // For now, erase the IP if it is there.
+         regex re(" ((25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])\\.){3}"
+                  "(25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])$");
+         s = regex_replace(s, re, (std::string)"");
+
+         // TODO: check the IP against the IP used to upload RISC-V source files
+      }
+      catch (std::exception& e)
+      {
+         cerr << e.what() << endl;
+      }
+      // output goes to socket
+      sout.rdbuf(bout_ptr);       
+   } else { // if we are not listening on a socket, get commands from terminal
+      cerr << ": " << std::flush;
+      s = readline(2); // 2 is stderr which in turn is stdin
+      // output goes to stderr
+      sout.rdbuf(cerr.rdbuf());
+   }
+   return s;
+}
+
+// write sout to socket (via bout)
+void sim_t::wout(streambuf *bout_ptr) {
+   if (acceptor_ptr) { // only if a socket has been created
+      try
+      {
+        boost::system::error_code ignored_error;
+        boost::asio::write(*socket_ptr, *bout_ptr, transfer_all(), ignored_error);
+        socket_ptr->close(); // close the socket after each command input/ouput
+                             //  This is need to in order to make the socket interface
+      }                      //  acessible by HTML GET via a socket client in a web server.
+      catch (std::exception& e)
+      {
+        cerr << e.what() << endl;
+      }
+   }
+}
+#endif
+
 void sim_t::interactive()
 {
-  typedef void (sim_t::*interactive_func)(const std::string&, const std::vector<std::string>&);
-  std::map<std::string,interactive_func> funcs;
+  typedef void (sim_t::*interactive_func)(const std::std::string&, const std::vector<std::std::string>&);
+  std::map<std::std::string,interactive_func> funcs;
 
   funcs["run"] = &sim_t::interactive_run_noisy;
   funcs["r"] = funcs["run"];
@@ -422,7 +493,7 @@ void sim_t::interactive_until(const std::string& cmd, const std::vector<std::str
   // mask bits above max_xlen
   int max_xlen = procs[strtol(args[1].c_str(),NULL,10)]->get_max_xlen();
   if (max_xlen == 32) val &= 0xFFFFFFFF;
-
+  
   std::vector<std::string> args2;
   args2 = std::vector<std::string>(args.begin()+1,args.end()-1);
 

Yihao23/spike_with_contract_dep_tracking

riscv/interactive.cc
@@ -8,6 +8,7 @@
 #include <termios.h>
 #include <map>
 #include <iostream>
+#include <iomanip>
 #include <climits>
 #include <cinttypes>
 #include <assert.h>
@@ -18,6 +19,19 @@
 #include <vector>
 #include <algorithm>
 #include <math.h>
+
+using std::ostream;
+using std::cerr;
+using std::hex;
+using std::dec;
+using std::setfill;
+using std::left;
+using std::right;
+using std::setw;
+using std::endl;
+
+#define STR_(X) #X      // these definitions allow to use a macro as a string
+#define STR(X) STR_(X)
 
 DECLARE_TRAP(-1, interactive)
 
@@ -57,6 +71,63 @@ static std::string readline(int fd)
   }
   return s;
 }
+
+#ifdef HAVE_BOOST_ASIO
+// read input command string
+std::string sim_t::rin(streambuf *bout_ptr) {
+   std::string s;
+   if (acceptor_ptr) { // if we are listening, get commands from socket
+      try
+      {
+         socket_ptr = new tcp::socket(*io_service_ptr);
+         acceptor_ptr->accept(*socket_ptr); // wait for someone to open connection
+         boost::asio::streambuf buf;
+         boost::asio::read_until(*socket_ptr, buf, "\n"); // wait for command
+         s = buffer_cast<const char*>(buf.data());
+         erase_all(s, "\r");  // get rid off any cr and lf
+         erase_all(s, "\n");
+         // The socket client is a web server and it appends the IP of the computer
+         // that sent the command from its web browser.
+
+         // For now, erase the IP if it is there.
+         regex re(" ((25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])\\.){3}"
+                  "(25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])$");
+         s = regex_replace(s, re, (std::string)"");
+
+         // TODO: check the IP against the IP used to upload RISC-V source files
+      }
+      catch (std::exception& e)
+      {
+         cerr << e.what() << endl;
+      }
+      // output goes to socket
+      sout.rdbuf(bout_ptr);
+   } else { // if we are not listening on a socket, get commands from terminal
+      cerr << ": " << std::flush;
+      s = readline(2); // 2 is stderr which in turn is stdin
+      // output goes to stderr
+      sout.rdbuf(cerr.rdbuf());
+   }
+   return s;
+}
+
+// write sout to socket (via bout)
+void sim_t::wout(streambuf *bout_ptr) {
+   if (acceptor_ptr) { // only if a socket has been created
+      try
+      {
+        boost::system::error_code ignored_error;
+        boost::asio::write(*socket_ptr, *bout_ptr, transfer_all(), ignored_error);
+        socket_ptr->close(); // close the socket after each command input/ouput
+                             //  This is need to in order to make the socket interface
+      }                      //  acessible by HTML GET via a socket client in a web server.
+      catch (std::exception& e)
+      {
+        cerr << e.what() << endl;
+      }
+   }
+}
+#endif
 
 void sim_t::interactive()
 {