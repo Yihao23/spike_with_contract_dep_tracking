riscv-software-src/riscv-isa-sim

commit 5b7cdbe1cf75112bd2a472b7490b15fa7078d798

DRY in selecting instruction functions

riscv/processor.cc
@@ -1027,11 +1027,11 @@ insn_func_t processor_t::decode_insn(insn_t insn)
   size_t idx = insn.bits() % OPCODE_CACHE_SIZE;
   insn_desc_t desc = opcode_cache[idx];
 
-  if (unlikely(insn.bits() != desc.match || !(xlen == 64 ? desc.rv64 : desc.rv32))) {
+  if (unlikely(insn.bits() != desc.match || !desc.func(xlen))) {
     // fall back to linear search
     int cnt = 0;
     insn_desc_t* p = &instructions[0];
-    while ((insn.bits() & p->mask) != p->match || !(xlen == 64 ? p->rv64 : p->rv32))
+    while ((insn.bits() & p->mask) != p->match || !desc.func(xlen))
       p++, cnt++;
     desc = *p;
 
@@ -1048,7 +1048,7 @@ insn_func_t processor_t::decode_insn(insn_t insn)
     opcode_cache[idx].match = insn.bits();
   }
 
-  return xlen == 64 ? desc.rv64 : desc.rv32;
+  return desc.func(xlen);
 }
 
 void processor_t::register_insn(insn_desc_t desc)

riscv/processor.h
@@ -29,6 +29,8 @@ struct insn_desc_t
   insn_bits_t mask;
   insn_func_t rv32;
   insn_func_t rv64;
+
+  insn_func_t func(int xlen) { return xlen == 64 ? rv64 : rv32; }
 };
 
 // regnum, data

Yihao23/spike_with_contract_dep_tracking

commit 4f0822515f86eeef849c2c251bb41522aed21429

Remove now-unnecessary null check from decode_insn
Fixes bug introduced in 5b7cdbe1cf75112bd2a472b7490b15fa7078d798

riscv/processor.cc
@@ -881,11 +881,11 @@ insn_func_t processor_t::decode_insn(insn_t insn)
 
   bool rve = extension_enabled('E');
 
-  if (unlikely(insn.bits() != desc.match || !desc.func(xlen, rve))) {
+  if (unlikely(insn.bits() != desc.match)) {
     // fall back to linear search
     int cnt = 0;
     insn_desc_t* p = &instructions[0];
-    while ((insn.bits() & p->mask) != p->match || !desc.func(xlen, rve))
+    while ((insn.bits() & p->mask) != p->match)
       p++, cnt++;
     desc = *p;
 