riscv-software-src/riscv-isa-sim

Add instruction limit
Adds an optional --instructions=N CLI argument which will stop the simulation after N instructions.

This is useful for benchmarking and profiling and sometimes debugging.

fesvr/htif.cc
@@ -46,7 +46,7 @@ static void handle_signal(int sig)
 
 htif_t::htif_t()
   : mem(this), entry(DRAM_BASE), sig_addr(0), sig_len(0),
-    tohost_addr(0), fromhost_addr(0), exitcode(0), stopped(false),
+    tohost_addr(0), fromhost_addr(0), stopped(false),
     syscall_proxy(this)
 {
   signal(SIGINT, &handle_signal);
@@ -116,7 +116,7 @@ std::map<std::string, uint64_t> htif_t::load_payload(const std::string& payload,
     else
       throw std::runtime_error(
         "could not open " + payload + "; searched paths:\n" +
-        "\t. (current directory)\n" + 
+        "\t. (current directory)\n" +
         "\t" + PREFIX TARGET_DIR + " (based on configured --prefix and --with-target)"
       );
   }
@@ -207,6 +207,14 @@ const char* htif_t::get_symbol(uint64_t addr)
   return it->second.c_str();
 }
 
+bool htif_t::should_exit() const {
+  return signal_exit || exitcode.has_value();
+}
+
+void htif_t::exit(int exit_code) {
+  exitcode = exit_code;
+}
+
 void htif_t::stop()
 {
   if (!sig_file.empty() && sig_len) // print final torture test signature
@@ -253,11 +261,11 @@ int htif_t::run()
     std::bind(enq_func, &fromhost_queue, std::placeholders::_1);
 
   if (tohost_addr == 0) {
-    while (!signal_exit)
+    while (!should_exit())
       idle();
   }
 
-  while (!signal_exit && exitcode == 0)
+  while (!should_exit())
   {
     uint64_t tohost;
 
@@ -305,7 +313,7 @@ bool htif_t::done()
 
 int htif_t::exit_code()
 {
-  return exitcode >> 1;
+  return exitcode.value_or(0) >> 1;
 }
 
 void htif_t::parse_arguments(int argc, char ** argv)

fesvr/htif.h
@@ -24,6 +24,9 @@ class htif_t : public chunked_memif_t
   virtual void start();
   virtual void stop();
 
+  // Cause the simulation to exit with the given exit code.
+  void exit(int exit_code);
+
   int run();
   bool done();
   int exit_code();
@@ -76,6 +79,10 @@ class htif_t : public chunked_memif_t
   // Given an address, return symbol from addr2symbol map
   const char* get_symbol(uint64_t addr);
 
+  // Return true if the simulation should exit due to a signal,
+  // or end-of-test from HTIF, or an instruction limit.
+  bool should_exit() const;
+
  private:
   void parse_arguments(int argc, char ** argv);
   void register_devices();
@@ -93,7 +100,8 @@ class htif_t : public chunked_memif_t
   addr_t sig_len; // torture
   addr_t tohost_addr;
   addr_t fromhost_addr;
-  int exitcode;
+  // Set to a value by exit() when the simulation should exit.
+  std::optional<int> exitcode;
   bool stopped;
 
   device_list_t device_list;

fesvr/syscall.cc
@@ -137,7 +137,7 @@ struct riscv_statx
       __spare2(), __spare3()
 #else
       __spare2()
-#endif      
+#endif
       {}
 };
 #endif
@@ -221,7 +221,7 @@ void syscall_t::handle_syscall(command_t cmd)
 
 reg_t syscall_t::sys_exit(reg_t code, reg_t a1, reg_t a2, reg_t a3, reg_t a4, reg_t a5, reg_t a6)
 {
-  htif->exitcode = code << 1 | 1;
+  htif->exit(code << 1 | 1);
   return 0;
 }
 

Yihao23/spike_with_contract_dep_tracking

fesvr/htif.cc
@@ -46,7 +46,7 @@ static void handle_signal(int sig)
 
 htif_t::htif_t()
   : mem(this), entry(DRAM_BASE), sig_addr(0), sig_len(0),
-    tohost_addr(0), fromhost_addr(0), exitcode(0), stopped(false),
+    tohost_addr(0), fromhost_addr(0), stopped(false),
     syscall_proxy(this)
 {
   signal(SIGINT, &handle_signal);
@@ -116,7 +116,7 @@ std::map<std::string, uint64_t> htif_t::load_payload(const std::string& payload,
     else
       throw std::runtime_error(
         "could not open " + payload + "; searched paths:\n" +
-        "\t. (current directory)\n" + 
+        "\t. (current directory)\n" +
         "\t" + PREFIX TARGET_DIR + " (based on configured --prefix and --with-target)"
       );
   }
@@ -207,6 +207,14 @@ const char* htif_t::get_symbol(uint64_t addr)
   return it->second.c_str();
 }
 
+bool htif_t::should_exit() const {
+  return signal_exit || exitcode.has_value();
+}
+
+void htif_t::htif_exit(int exit_code) {
+  exitcode = exit_code;
+}
+
 void htif_t::stop()
 {
   if (!sig_file.empty() && sig_len) // print final torture test signature
@@ -253,11 +261,11 @@ int htif_t::run()
     std::bind(enq_func, &fromhost_queue, std::placeholders::_1);
 
   if (tohost_addr == 0) {
-    while (!signal_exit)
+    while (!should_exit())
       idle();
   }
 
-  while (!signal_exit && exitcode == 0)
+  while (!should_exit())
   {
     uint64_t tohost;
 
@@ -305,7 +313,7 @@ bool htif_t::done()
 
 int htif_t::exit_code()
 {
-  return exitcode >> 1;
+  return exitcode.value_or(0) >> 1;
 }
 
 void htif_t::parse_arguments(int argc, char ** argv)

fesvr/htif.h
@@ -24,6 +24,9 @@ class htif_t : public chunked_memif_t
   virtual void start();
   virtual void stop();
 
+  // Cause the simulation to exit with the given exit code.
+  void htif_exit(int exit_code);
+
   int run();
   bool done();
   int exit_code();
@@ -76,6 +79,10 @@ class htif_t : public chunked_memif_t
   // Given an address, return symbol from addr2symbol map
   const char* get_symbol(uint64_t addr);
 
+  // Return true if the simulation should exit due to a signal,
+  // or end-of-test from HTIF, or an instruction limit.
+  bool should_exit() const;
+
  private:
   void parse_arguments(int argc, char ** argv);
   void register_devices();
@@ -93,7 +100,8 @@ class htif_t : public chunked_memif_t
   addr_t sig_len; // torture
   addr_t tohost_addr;
   addr_t fromhost_addr;
-  int exitcode;
+  // Set to a value by htif_exit() when the simulation should exit.
+  std::optional<int> exitcode;
   bool stopped;
 
   device_list_t device_list;

fesvr/syscall.cc
@@ -137,7 +137,7 @@ struct riscv_statx
       __spare2(), __spare3()
 #else
       __spare2()
-#endif      
+#endif
       {}
 };
 #endif
@@ -221,7 +221,7 @@ void syscall_t::handle_syscall(command_t cmd)
 
 reg_t syscall_t::sys_exit(reg_t code, reg_t a1, reg_t a2, reg_t a3, reg_t a4, reg_t a5, reg_t a6)
 {
-  htif->exitcode = code << 1 | 1;
+  htif->htif_exit(code << 1 | 1);
   return 0;
 }
 